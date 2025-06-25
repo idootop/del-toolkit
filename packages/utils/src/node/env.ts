8b7d96fb297d1dfc988082c454ddb3c3f9a035fc
Del Wang <wjg12341234@163.com>
2025-06-25 11:36:59 +0800

    fix: kIsNode

--- packages/utils/src/node/env.ts
+++ packages/utils/src/node/env.ts
@@ -4,4 +4,5 @@ export const kIsNodeDev = process.env.NODE_ENV === 'development';
 
 export const kIsNodeProd = process.env.NODE_ENV === 'production';
 
-export const kIsNode = typeof process !== 'undefined' && process.versions.node;
+export const kIsNode =
+  typeof process !== 'undefined' && !!process.versions?.node;
